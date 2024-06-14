c4d6be47d3218a9a56a12f3b04b0afa8d8592846
Lynette <90663407+LynetteLiu2333@users.noreply.github.com>
2024-06-14 12:39:31 -0600

    Use local storage instead of cookies

--- cartUtils.js
+++ cartUtils.js
@@ -1,16 +1,3 @@
-// // Retrieve cart data from cookies
-// export function getCartFromCookies() {
-//     const cookieValue = document.cookie
-//         .split("; ")
-//         .find(row => row.startsWith("list-cart="));
-//     return cookieValue ? JSON.parse(cookieValue.split("=")[1]) : [];
-// }
-
-// // Update cart data in cookies
-// export function updateCartCookies(listCart) {
-//     document.cookie = `list-cart=${JSON.stringify(listCart)}; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;`;
-// }
-
 // Retrieve cart data from local storage
 export function getCartFromLocalStorage() {
     const cart = localStorage.getItem("list-cart");
